1a56c1167cf8ce271ba4c23bbda21182bb3ab3b5
Toshit Chawda <r58playz@gmail.com>
2023-09-17 05:33:27 -0700

    dynamic

--- static/proxyHelpers.js
+++ static/proxyHelpers.js
@@ -1,6 +1,8 @@
 function proxyUsing(url, proxy, callback) {
     if (proxy === "UV") {
         proxyUsingUV(url, callback);
+    } else if (proxy === "Dynamic") {
+        proxyUsingDynamic(url, callback);
     } else {
         console.error("Invalid proxy!");
     }
@@ -9,6 +11,8 @@ function proxyUsing(url, proxy, callback) {
 function baseUrlFor(proxy) {
     if (proxy === "UV") {
         return __uv$config.prefix;
+    } else if (proxy === "Dynamic") {
+        return __dynamic$config.prefix; 
     } else {
         console.error("Invalid proxy!");
     }
@@ -16,6 +20,8 @@ function baseUrlFor(proxy) {
 
 function decodeUrl(url, proxy) {
     if (proxy === "UV") {
+        return __uv$config.decodeUrl(url);
+    } else if (proxy === "Dynamic") {
         return __uv$config.decodeUrl(url);
     } else {
         console.error("Invalid proxy!");
@@ -25,6 +31,8 @@ function decodeUrl(url, proxy) {
 function encodeUrl(url, proxy) {
     if (proxy === "UV") {
         return __uv$config.encodeUrl(url);
+    } else if (proxy === "Dynamic") {
+        return __uv$config.encodeUrl(url);
     } else {
         console.error("Invalid proxy!");
     }
@@ -33,5 +41,11 @@ function encodeUrl(url, proxy) {
 function proxyUsingUV(url, callback) {
     window.navigator.serviceWorker.register('./sw.js', {scope: "/service"}).then(() => {
         callback(baseUrlFor("UV") + encodeUrl(url, "UV"));
+    });
+}
+
+function proxyUsingDynamic(url, callback) {
+    window.navigator.serviceWorker.register('./sw.js', {scope: "/service"}).then(() => {
+        callback(baseUrlFor("Dynamic") + encodeUrl(url, "Dynamic"));
     });
 }
--- static/sw.js
+++ static/sw.js
@@ -2,10 +2,21 @@ importScripts('./uv/uv.bundle.js');
 importScripts('./uv/uv.config.js');
 importScripts('./uv/uv.sw.js');
 
+importScripts('./dynamic/dynamic.config.js');
+importScripts('./dynamic/dynamic.worker.js');
+
 const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
 self.addEventListener('fetch', event => {
-    if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
-        event.respondWith(uv.fetch(event))
-    }
+    event.respondWith(
+        (async ()=>{
+            if(event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+                return await uv.fetch(event);
+            } else if(await dynamic.route(event)) {
+                return await dynamic.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
+    );
 });
