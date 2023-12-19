0ab9fc634cb993c62d2f03fae7303d420b105a88
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-12-19 16:31:21 -0500

    anura ver

--- static/extensions/resources.js
+++ static/extensions/resources.js
@@ -6,14 +6,14 @@ class ExtensionResources {
     this.resourcesUrl = window.location.origin + "/extension";
   }
 
-  static new(fsName = "aboutproxy-extensions") {
+  static new(fsName = "local") {
     // least confusing code
     // it's what? 2 promises nested? no 3
     // 1
     return new Promise((resolve, reject) => {
       let filesystem = new Filer.FileSystem({ name: fsName }, (err, fs) => {
         if(err) reject(err);
-        let cls = new ExtensionResources(fs);
+        let cls = new ExtensionResources(Filer.fs);
         cls.setUpSw().then(
           ()=>{resolve(cls)},
           (val)=>{reject(val)}
@@ -22,8 +22,10 @@ class ExtensionResources {
     });
   }
 
-  setUpSw() {
-    return window.navigator.serviceWorker.register("/nohost-sw.js?route=extension&fsName=aboutproxy-extensions");
+  async setUpSw() {
+    // Stub
+    return; 
+    // return window.navigator.serviceWorker.register("/nohost-sw.js?route=extension&fsName=aboutproxy-extensions");
   }
 }
 
